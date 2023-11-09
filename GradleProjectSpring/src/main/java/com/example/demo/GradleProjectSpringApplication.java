daf10a4b722a2ccd4f5c002c1e517912432c5279
JuankaEsMa <joancarles.escobar@gmail.com>
2023-11-09 22:41:47 +0100

    first commit

---
+++ GradleProjectSpring/src/main/java/com/example/demo/GradleProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleProjectSpringApplication.class, args);
+	}
+
+}
